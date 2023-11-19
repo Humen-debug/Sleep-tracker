6fa2edc7a89faac60468022cee30be488c2945bd
humenchau <humenchau@gmail.com>
2023-11-19 18:11:19 +0800

    update fab in diary

--- lib/pages/settings/sleep_diary.dart
+++ lib/pages/settings/sleep_diary.dart
@@ -1,25 +1,37 @@
-import 'dart:math';
+import 'dart:math' as math;
 
 import 'package:auto_route/auto_route.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/intl.dart';
 import 'package:sleep_tracker/components/moods/utils.dart';
 import 'package:sleep_tracker/components/sleep_phase_block.dart';
+import 'package:sleep_tracker/models/sleep_record.dart';
+import 'package:sleep_tracker/providers/sleep_records_provider.dart';
 import 'package:sleep_tracker/utils/style.dart';
 import 'package:sleep_tracker/utils/theme_data.dart';
 
 @RoutePage()
-class SleepDiaryPage extends StatefulWidget {
+class SleepDiaryPage extends ConsumerStatefulWidget {
   const SleepDiaryPage({super.key});
 
   @override
-  State<SleepDiaryPage> createState() => _SleepDiaryPageState();
+  ConsumerState<SleepDiaryPage> createState() => _SleepDiaryPageState();
 }
 
-class _SleepDiaryPageState extends State<SleepDiaryPage> {
+class _SleepDiaryPageState extends ConsumerState<SleepDiaryPage> {
   final ScrollController _scrollController = ScrollController();
+  final int _dayToGenerate = 30 * 3;
+  bool _showFab = false;
+  final Duration _fabAnimationDuration = const Duration(milliseconds: 300);
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_handleFabListener);
+  }
 
   @override
   void dispose() {
@@ -27,13 +39,32 @@ class _SleepDiaryPageState extends State<SleepDiaryPage> {
     super.dispose();
   }
 
-  Widget _buildItems(BuildContext context, int index) {
-    final DateTime date = DateTime.now().subtract(Duration(days: index));
-    final ThemeData themeData = Theme.of(context);
-    final double moodValue = Random().nextDouble();
+  void _handleFabListener() {
+    final double height = MediaQuery.sizeOf(context).height;
+    final double offset = _scrollController.offset;
+    if (!_showFab && offset > height) {
+      setState(() => _showFab = true);
+    } else if (_showFab && offset <= height) {
+      setState(() => _showFab = false);
+    }
+  }
 
+  Widget _buildItems(BuildContext context, int index) {
+    final ThemeData themeData = Theme.of(context);
+
+    final DateTime date = DateTime.now().subtract(Duration(days: index));
+    final Iterable<SleepRecord> records = ref.watch(daySleepRecordsProvider(date));
+    final moods = records.map((record) => record.sleepQuality).whereNotNull();
+    final double? moodValue = moods.isNotEmpty ? moods.average : null;
+    final List<DateTimeRange> slots = records
+        .where((record) => record.wakeUpAt != null)
+        .map((record) => DateTimeRange(start: record.start, end: record.wakeUpAt!))
+        .toList();
+
+    if (records.isEmpty) return const SizedBox.shrink();
     return Container(
       padding: const EdgeInsets.symmetric(vertical: Style.spacingXs, horizontal: Style.spacingMd),
+      margin: const EdgeInsets.only(bottom: Style.spacingSm),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(Style.radiusSm),
         gradient: LinearGradient(
@@ -87,13 +118,15 @@ class _SleepDiaryPageState extends State<SleepDiaryPage> {
                   ],
                 ),
               ),
-              const SizedBox(width: Style.spacingLg),
-              Container(
-                  padding: const EdgeInsets.all(2),
-                  decoration: const BoxDecoration(shape: BoxShape.circle, color: Style.grey1),
-                  child: Center(
-                    child: SvgPicture.asset('assets/moods/${valueToMood(moodValue).name}.svg', width: 32, height: 32),
-                  ))
+              if (moodValue != null) ...[
+                const SizedBox(width: Style.spacingLg),
+                Container(
+                    padding: const EdgeInsets.all(2),
+                    decoration: const BoxDecoration(shape: BoxShape.circle, color: Style.grey1),
+                    child: Center(
+                      child: SvgPicture.asset('assets/moods/${valueToMood(moodValue).name}.svg', width: 32, height: 32),
+                    ))
+              ]
             ],
           ),
           // dev
@@ -101,18 +134,10 @@ class _SleepDiaryPageState extends State<SleepDiaryPage> {
             padding: const EdgeInsets.symmetric(vertical: Style.spacingLg),
             child: _TimeSlotChart(
               range: DateTimeRange(
-                  start: DateUtils.dateOnly(date.subtract(const Duration(days: 1))).copyWith(hour: 7),
-                  end: DateUtils.dateOnly(date).copyWith(hour: 14)),
-              slots: [
-                DateTimeRange(
-                  start: date.copyWith(hour: 7, minute: 30),
-                  end: date.copyWith(hour: 14),
-                ),
-                DateTimeRange(
-                  start: DateUtils.dateOnly(date.subtract(const Duration(days: 1))).copyWith(hour: 15),
-                  end: DateUtils.dateOnly(date.subtract(const Duration(days: 1))).copyWith(hour: 15, minute: 20),
-                )
-              ],
+                start: (records.first.start),
+                end: (records.last.wakeUpAt ?? records.last.end),
+              ),
+              slots: slots,
             ),
           ),
           Row(
@@ -134,15 +159,19 @@ class _SleepDiaryPageState extends State<SleepDiaryPage> {
       appBar: AppBar(title: const Text('Sleep Diary')),
       floatingActionButton: Padding(
         padding: const EdgeInsets.only(bottom: kBottomNavigationBarHeight),
-        child: FloatingActionButton(
-          onPressed: () {
-            _scrollController.animateTo(0, duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
-          },
-          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-          elevation: 8,
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(Style.radiusSm)),
-          child:
-              SvgPicture.asset('assets/icons/to-top.svg', color: Theme.of(context).primaryColor, width: 32, height: 32),
+        child: AnimatedSlide(
+          offset: _showFab ? Offset.zero : const Offset(0, 2),
+          duration: _fabAnimationDuration,
+          child: FloatingActionButton(
+            onPressed: () {
+              _scrollController.animateTo(0, duration: const Duration(milliseconds: 500), curve: Curves.easeInOut);
+            },
+            backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+            elevation: 8,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(Style.radiusSm)),
+            child: SvgPicture.asset('assets/icons/to-top.svg',
+                color: Theme.of(context).primaryColor, width: 32, height: 32),
+          ),
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
@@ -153,13 +182,12 @@ class _SleepDiaryPageState extends State<SleepDiaryPage> {
         controller: _scrollController,
         child: Column(
           children: [
-            ListView.separated(
+            ListView.builder(
               shrinkWrap: true,
               physics: const NeverScrollableScrollPhysics(),
               padding: EdgeInsets.zero,
               itemBuilder: _buildItems,
-              separatorBuilder: (_, __) => const SizedBox(height: Style.spacingSm),
-              itemCount: 18,
+              itemCount: _dayToGenerate,
             ),
             Divider(color: Theme.of(context).colorScheme.tertiary, height: Style.spacingSm * 2),
             Text(
@@ -183,9 +211,9 @@ class _SleepDiaryPageState extends State<SleepDiaryPage> {
 /// and end is in the format of Md.
 class _TimeSlotChart extends StatelessWidget {
   _TimeSlotChart({required this.range, required this.slots}) {
-    assert(!slots.sorted((a, b) => a.start.compareTo(b.start)).last.start.isBefore(range.start),
+    assert(slots.isEmpty || !slots.sorted((a, b) => a.start.compareTo(b.start)).last.start.isBefore(range.start),
         'slot must be on or after ${range.start}.');
-    assert(!slots.sorted((a, b) => a.end.compareTo(b.end)).last.end.isBefore(range.start),
+    assert(slots.isEmpty || !slots.sorted((a, b) => a.end.compareTo(b.end)).last.end.isBefore(range.start),
         'slot must be on or before ${range.end}.');
   }
 
@@ -200,13 +228,14 @@ class _TimeSlotChart extends StatelessWidget {
     final int startMilliSecond = range.start.millisecondsSinceEpoch;
     final int endMilliSecond = range.end.millisecondsSinceEpoch;
 
+    if ((endMilliSecond - startMilliSecond) == 0) return 0;
     return (milliSecond - startMilliSecond) / (endMilliSecond - startMilliSecond);
   }
 
   Widget _buildChart(BuildContext context, BoxConstraints constraints) {
     final BorderRadius borderRadius = BorderRadius.circular(2);
     return Container(
-      constraints: constraints.copyWith(minHeight: 8),
+      constraints: constraints.copyWith(minHeight: 8, maxWidth: constraints.maxWidth),
       decoration: BoxDecoration(borderRadius: borderRadius),
       clipBehavior: Clip.hardEdge,
       child: Stack(
@@ -217,11 +246,12 @@ class _TimeSlotChart extends StatelessWidget {
           ...slots.map((slot) {
             final double left = constraints.maxWidth * _computeHorizontalPercent(slot.start);
             final double right = constraints.maxWidth * _computeHorizontalPercent(slot.end);
+
             return Positioned(
               top: 0,
               bottom: 0,
               left: left,
-              width: right - left,
+              width: math.min(right - left, constraints.maxWidth - left),
               child: Container(
                 decoration: BoxDecoration(borderRadius: borderRadius, color: Theme.of(context).primaryColor),
               ),
